c96813955dc4eb48b76601792766fd75043342c6
hou5emd <andrandrey92@gmail.com>
2024-11-10 00:34:51 +0500

    update linter

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,24 +1,20 @@
 import pluginJs from '@eslint/js';
-import tsPlugin from '@typescript-eslint/eslint-plugin';
+import stylisticJs from '@stylistic/eslint-plugin';
+import deprecatedTsPlugin from '@typescript-eslint/eslint-plugin';
 import tsParser from '@typescript-eslint/parser';
 import destructuringPlugin from 'eslint-plugin-destructuring';
 import importPlugin from 'eslint-plugin-import';
 import jsxA11y from 'eslint-plugin-jsx-a11y';
 import importPath from 'eslint-plugin-no-relative-import-paths';
-import prettierRecommended from 'eslint-plugin-prettier/recommended';
-import pluginReact from 'eslint-plugin-react';
+import prettier from 'eslint-plugin-prettier';
+import deprecatedPluginReact from 'eslint-plugin-react';
 import globals from 'globals';
-
-import airBnbBase from './__assets__/eslint-airbnb/index.mjs';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'], ignores: ['src/api/generated/**/*.ts'] },
   { languageOptions: { globals: { ...globals.node, ...globals.browser } } },
   pluginJs.configs.recommended,
-  pluginReact.configs.flat.recommended,
-  pluginReact.configs.flat['jsx-runtime'],
-  prettierRecommended,
   {
     languageOptions: {
       parser: tsParser,
@@ -28,11 +24,14 @@ export default [
       },
     },
     plugins: {
-      '@typescript-eslint': tsPlugin,
       import: importPlugin,
       'no-relative-import-paths': importPath,
       'jsx-a11y': jsxA11y,
       destructuring: destructuringPlugin,
+      deprecatedReact: deprecatedPluginReact,
+      '@stylistic': stylisticJs,
+      deprecatedTs: deprecatedTsPlugin,
+      prettier,
     },
     settings: {
       'import/resolver': {
@@ -40,21 +39,20 @@ export default [
       },
     },
     rules: {
-      ...airBnbBase,
-      '@typescript-eslint/ban-ts-comment': 'off',
-      '@typescript-eslint/no-base-to-string': 'off',
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': 'off',
-      '@typescript-eslint/no-misused-promises': 'off',
-      '@typescript-eslint/no-non-null-assertion': 'error',
-      '@typescript-eslint/no-redundant-type-constituents': 'off',
-      '@typescript-eslint/no-restricted-imports': ['error', { paths: ['preact'], patterns: ['preact/*'] }],
-      '@typescript-eslint/no-shadow': 'off',
-      '@typescript-eslint/no-unsafe-enum-comparison': 'off',
-      '@typescript-eslint/no-unused-vars': 'warn',
-      '@typescript-eslint/no-use-before-define': 'warn',
-      '@typescript-eslint/restrict-template-expressions': 'off',
-      '@typescript-eslint/switch-exhaustiveness-check': 'error',
+      '@stylistic/ban-ts-comment': 'off',
+      '@stylistic/no-base-to-string': 'off',
+      '@stylistic/no-explicit-any': 'off',
+      '@stylistic/no-floating-promises': 'off',
+      '@stylistic/no-misused-promises': 'off',
+      'deprecatedTs/no-non-null-assertion': 'error',
+      '@stylistic/no-redundant-type-constituents': 'off',
+      'deprecatedTs/no-restricted-imports': ['error', { paths: ['preact'], patterns: ['preact/*'] }],
+      '@stylistic/no-shadow': 'off',
+      '@stylistic/no-unsafe-enum-comparison': 'off',
+      'deprecatedTs/no-unused-vars': 'warn',
+      'deprecatedTs/no-use-before-define': 'warn',
+      '@stylistic/restrict-template-expressions': 'off',
+      'deprecatedTs/switch-exhaustiveness-check': 'error',
       'class-methods-use-this': 'off',
       'destructuring/in-methods-params': 'error',
       'destructuring/in-params': ['error', { 'max-params': 0 }],
@@ -86,18 +84,26 @@ export default [
       'no-relative-import-paths/no-relative-import-paths': ['error', { allowSameFolder: true, rootDir: 'src' }],
       'no-shadow': 'off',
       'no-use-before-define': 'off',
-      'react/display-name': 'off',
-      'react/function-component-definition': 'off',
-      'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
-      'react/jsx-props-no-spreading': 'off',
-      'react/jsx-no-useless-fragment': ['off'],
-      'react/jsx-wrap-multilines': ['error', { return: 'parens-new-line' }],
-      'react/no-array-index-key': 'off',
-      'react/no-unstable-nested-components': 'off',
-      'react/prop-types': 'off',
-      'react/require-default-props': 'off',
+      '@stylistic/display-name': 'off',
+      '@stylistic/function-component-definition': 'off',
+      'deprecatedReact/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+      '@stylistic/jsx-props-no-spreading': 'off',
+      '@stylistic/jsx-no-useless-fragment': ['off'],
+      '@stylistic/jsx-wrap-multilines': ['error', { return: 'parens-new-line' }],
+      '@stylistic/no-array-index-key': 'off',
+      '@stylistic/no-unstable-nested-components': 'off',
+      '@stylistic/prop-types': 'off',
+      '@stylistic/require-default-props': 'off',
       'react-hooks/exhaustive-deps': 'off',
-      'react/react-in-jsx-scope': 'off',
+      '@stylistic/react-in-jsx-scope': 'off',
+      'prettier/prettier': [
+        'error',
+        {
+          singleQuote: true,
+          printWidth: 100,
+          tabWidth: 2,
+        },
+      ],
     },
   },
 ];
